0047da58479a2fc2e43e75fb4148bdb70cb5398c
Xtremy <rajulverma7777@gmail.com>
2025-06-25 17:35:29 +0530

    added type for loadash

--- src/components/SearchFilter.tsx
+++ src/components/SearchFilter.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import React, { useState, useEffect, useCallback } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { motion } from "framer-motion";
